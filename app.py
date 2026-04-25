d0cb0b3f3dadf834355799e5eb53aa07cd09fa91
Sabari4100 <naveensabari4100.com>
2026-04-25 16:10:43 +0530

    This is Commit for Web page readiness

--- app.py
+++ app.py
@@ -20,5 +20,13 @@ def predict_api():
     print (output[0])
     return jsonify(output[0])
 
+@app.route("/predict",methods = ['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_data = scalar.transform(np.array(data).reshape(1,-1))
+    print(final_data)
+    output = reg_model.predict(final_data)[0]
+    return render_template("home.html",prediction_text = "The predicted House Price is {}".format(output))
+
 if __name__ == "__main__":
     app.run(debug=True)
